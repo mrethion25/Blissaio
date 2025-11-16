e3ddf6c871c09f8118b37f7fa3a6ba5295b6ac9e
mrethion25 <worldof2512@gmail.com>
2025-11-16 20:33:02 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
-import { LanguageProvider } from './context/LanguageContext';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App.jsx';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ClerkProvider } from "@clerk/clerk-react";
 
-createRoot(document.getElementById('root')).render(
-  <LanguageProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </LanguageProvider>
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+    <App />
+  </ClerkProvider>
 );
